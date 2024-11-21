56c5dd71e4982b9ab5a0505ebe7a1798fe558542
limkanghyun <limkh06910691@gmail.com>
2024-11-22 00:00:40 +0900

    Servlet 회원가입, 회원목록 조회 웹

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();//Singleton 이므로 new 사용 안됨
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();//테스트가 끝날때마다 테스트를 초기화(테스트시 꼭 필요!)
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
