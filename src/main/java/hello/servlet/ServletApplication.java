9885157d294d30a404f06c817afdb5c26e7e7fb1
limkanghyun <limkh06910691@gmail.com>
2024-11-28 22:51:52 +0900

    애노테이션 활용하여 MVC패턴 회원가입, 회원목록 조회 웹

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,13 +1,21 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan//서블릿 자동등록
 @SpringBootApplication
 public class ServletApplication {
 
-	public static void main(String[] args) { SpringApplication.run(ServletApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(ServletApplication.class, args);}
+
+	/*
+	//@Component 없이 직접 Bean등록
+	@Bean
+	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+		return new SpringMemberFormControllerV1();
+	}*/
 }
